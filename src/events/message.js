47f3863d02470b332816584e689fe216008792ee
William Holmes <holmesw@ie.ibm.com>
2018-11-12 15:14:21 +0000

    Ignore bot message

--- src/events/message.js
+++ src/events/message.js
@@ -2,11 +2,12 @@ const web = require('../webClient');
 
 const message = (...args) => {
   console.log('MESSAGE Event', args);
-  const [{ user, channel, text, subtype, files = [] }] = args;
+  const [{ bot_id, channel, text }] = args;
 
-    if (!user || subtype) {
-      return;
-    }
+  if(_.isEqual(bot_id, 'BDX72S2JZ')) {
+    console.log('IGNORE My Bot Message');
+    return
+  }
 
     web.chat.postMessage({ channel, text }).then(slackResponse => {
         console.log('Message sent: ' + slackResponse.ts);
