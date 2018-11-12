8e3866ca9dd7b6aac1dd78b2ceccddad1b847807
William Holmes <holmesw@ie.ibm.com>
2018-11-12 12:13:28 +0000

    Update Actions

--- src/handlers/actions/index.js
+++ src/handlers/actions/index.js
@@ -2,7 +2,7 @@ const queryStrings = require('query-string');
 
 const interactiveMessage = require('./interactiveMessage');
 
-const web = require('../webClient');
+const web = require('../../webClient');
 
 const actions = (req, res) => {
     const body = queryStrings.parse(req.body.toString());
