16432db462f54540baa2ee315fd231d02ecac731
William Holmes <holmesw@ie.ibm.com>
2018-11-13 11:20:14 +0000

    Update unfurl Links

--- src/events/link.js
+++ src/events/link.js
@@ -1,35 +1,40 @@
-const grabity = require("grabity");
+const got = require('got');
 const _ = require('underscore');
 const Q = require('q');
 
+const metascraper = require('metascraper')([
+  require('metascraper-description')(),
+  require('metascraper-image')(),
+  require('metascraper-title')(),
+  require('metascraper-url')()
+]);
+
 const web = require('../webClient');
 
+const getAttachment = ({ title, description, image, url }) => ({
+  url,
+  title,
+  title_link: image,
+  thumb_url: image,
+  fields: [{
+    title: 'Description',
+    value: description
+  }]
+});
+
+const getUnfurls = (data) => _.chain(data).map(getAttachment).indexBy('url').value();
+
 const message = (...args) => {
-  console.log('LINK Event', args);
   const [{ channel, message_ts: ts, links = [] }] = args;
+
   const promises = _.map(links, async ({ url }) => {
-    const meta = await grabity.grabIt(url);
-    return{ ...meta, url };
+    const { body: html, url: link } = await got(url);
+    return { ...(await metascraper({ html, url: link })), url };
   });
+
   return Q.all(promises).then(data => {
-    const unfurls = _.chain(data)
-      .map(({ title, description, image, url }) => ({
-        url,
-        title,
-        title_link: image,
-        image_url: image,
-        fields: [{
-          title: 'Description',
-          value: description
-        }]
-      }))
-      .indexBy('url')
-      .value();
-
-    web.chat.unfurl({ channel, ts, unfurls }).then(slackResponse => {
-      console.log(`Message sent: ${slackResponse.ts}`);
-    }).catch(console.error);
-
+    const unfurls = getUnfurls(data, 'url');
+    web.chat.unfurl({ channel, ts, unfurls }).catch(console.error);
   });
 };
 
