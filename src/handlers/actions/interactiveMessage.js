dceb047ccdddffb620d910168ae6b4be17608bea
William Holmes <holmesw@ie.ibm.com>
2018-11-13 16:02:30 +0000

    update indentation

--- src/handlers/actions/interactiveMessage.js
+++ src/handlers/actions/interactiveMessage.js
@@ -9,17 +9,15 @@ const interactiveMessage = (req, res) => {
     const { channel: { id: channel }, message_ts: ts } = payload;
 
     const content = {
-      "text": "Try out these buttons",
-      "attachments": [
-          {
-            "title": "Feeback",
-            "text" : "Thanks for the Feeback <@UDW87UF6U>"
-          }
-        ]
-      }
-    web.chat.update({ channel, ts, ...content}).then(slackResponse => {
-      console.log(`Message sent: ${slackResponse.ts}`);
-    }).catch(console.error);
+        text: 'Try out these buttons',
+        attachments: [{
+            title: 'Feeback',
+            text : 'Thanks for the Feeback <@UDW87UF6U>'
+        }]
+    }
+
+    web.chat.update({ channel, ts, ...content}).catch(console.error);
+
     return res.send();
 }
 
--- src/handlers/options/dialogSuggestions.js
+++ src/handlers/options/dialogSuggestions.js
@@ -1,26 +1,19 @@
 const dialogSuggestions = (req, res) => {
-    return res.send({
-        "option_groups": [
-        {
-          "label": "A",
-          "options": [
-            {
-              "label": "Accountant",
-              "value": "accountant"
-            }
-          ]
-        },
-        {
-          "label": "B",
-          "options": [
-            {
-              "label": "Barista",
-              "value": "barista"
-            }
-          ]
-        }
-      ]
-    });
+  return res.send({
+    option_groups: [{
+      label: "A",
+      options: [{
+        label: "Accountant",
+        value: "accountant"
+      }]
+    }, {
+      label: "B",
+      options: [{
+        label: "Barista",
+        value: "barista"
+      }]
+    }]
+  });
 };
 
 module.exports = dialogSuggestions;
